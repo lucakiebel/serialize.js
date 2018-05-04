54e0c496cfdab84271200cc347809f5a3ec4bb30
Luca Kiebel <github@luca-kiebel.de>
2018-05-04 13:52:32 +0200

    Create serialize.js

---
+++ serialize.js
@@ -0,0 +1,65 @@
+function serialize(form, spaceToPlus = false) {
+  if (!form || form.nodeName !== "FORM") {
+    return;
+  }
+  let i, j, q = [];
+  for (i = form.elements.length - 1; i >= 0; i = i - 1) {
+    if (form.elements[i].name === "") {
+      continue;
+    }
+    switch (form.elements[i].nodeName) {
+      case 'INPUT':
+        switch (form.elements[i].type) {
+          case 'text':
+          case 'hidden':
+          case 'password':
+          case 'email':
+          case 'number':
+          case 'button':
+          case 'reset':
+          case 'submit':
+            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            break;
+          case 'checkbox':
+          case 'radio':
+            if (form.elements[i].checked) {
+              q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            }
+            break;
+          case 'file':
+            break;
+        }
+        break;
+      case 'TEXTAREA':
+        q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value))
+        break;
+      case 'SELECT':
+        switch (form.elements[i].type) {
+          case 'select-one':
+            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            break;
+          case 'select-multiple':
+            for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+              if (form.elements[i].options[j].selected) {
+                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
+              }
+            }
+            break;
+        }
+        break;
+      case 'BUTTON':
+        switch (form.elements[i].type) {
+          case 'reset':
+          case 'submit':
+          case 'button':
+            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            break;
+        }
+        break;
+    }
+  }
+  if (spaceToPlus === true) {
+    q = q.map(e => e.replace("%20", "+"));
+  }
+  return q.join("&");
+}
